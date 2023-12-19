6e96bc8bf52afaad4cbc1a08d8fa391c21396ec8
Terry Parrish <tparrish@Terrys-MBP.attlocal.net>
2023-12-19 18:11:32 -0500

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,37 +3,37 @@
 import { useGetUsersQuery } from "@/redux/services/userApi";
 import { decrement, increment, reset } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import {useTranslations} from 'next-intl';
+import { useTranslations } from "next-intl";
 
 export default function Home() {
   const count = useAppSelector((state) => state.counterReducer.value);
   const dispatch = useAppDispatch();
 
   const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
-  const t = useTranslations('Index');
+  const t = useTranslations("Index");
 
   return (
     <main className="p-[20px] max-w-[1200px] ms-auto me-auto">
       <div className="mb-16 text-center">
         <h4 className="mb-4">{count}</h4>
-        <button onClick={() => dispatch(increment())}>{t('increment')}&nbsp;</button>
+        <button onClick={() => dispatch(increment())}>
+          {t("increment")}&nbsp;
+        </button>
         <button
           onClick={() => dispatch(decrement())}
           className="ms-auto me-auto"
         >
-          {t('decrement')}&nbsp;
+          {t("decrement")}&nbsp;
         </button>
-        <button onClick={() => dispatch(reset())}>{t('reset')}&nbsp;</button>
+        <button onClick={() => dispatch(reset())}>{t("reset")}&nbsp;</button>
       </div>
 
       {error ? (
-        <p>{t('error_message')}&nbsp;</p>
+        <p>{t("error_message")}&nbsp;</p>
       ) : isLoading || isFetching ? (
-        <p>{t('loading')}&nbsp;...</p>
+        <p>{t("loading")}&nbsp;...</p>
       ) : data ? (
-        <div 
-        className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-[20px]"
-        >
+        <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-[20px]">
           {data.map((user) => (
             <div
               className="text-center border-solid border-[#ccc] border-[1px]"
